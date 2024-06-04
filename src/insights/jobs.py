d6181ac9151d7bde6c1af2f0619a81dede214406
Izadora Braga <izadora_abraga@outlook.com>
2024-06-04 20:20:55 -0300

    requisito 4

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -20,5 +20,16 @@ class ProcessJobs:
                 job_types.add(job_type)
         return list(job_types)
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+        self, jobs: List[Dict], filter_criteria: Dict
+    ) -> List[dict]:
+        if not isinstance(filter_criteria, dict):
+            raise TypeError(
+                "O filtro fornecido para o método "
+                "filter_by_multiple_criteria não é um dicionário."
+            )
+        return [
+            job for job in jobs
+            if all(job.get(key) == value for key, value
+                   in filter_criteria.items())
+        ]
