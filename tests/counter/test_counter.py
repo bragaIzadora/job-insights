fd20267050f7f7603857d6ffb85e79d9552e0df6
Izadora Braga <izadora_abraga@outlook.com>
2024-06-04 18:23:16 -0300

    iniciando, testando requisito 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    path = "data/jobs.csv"
+    word = "Python"
+
+    result = count_ocurrences(path, word)
+
+    assert isinstance(result, int), "O resultado deve ser um inteiro"
+
+    result_upper = count_ocurrences(path, word.upper())
+    assert result == result_upper, "A contagem de palavras deve ser insensível a maiúsculas e minúsculas"
