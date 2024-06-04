00542cbee86361d388fbc8a5943a030bda625aec
Izadora Braga <izadora_abraga@outlook.com>
2024-06-04 18:35:40 -0300

    requisito 1 ajustes

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -7,7 +7,6 @@ def test_counter():
 
     result = count_ocurrences(path, word)
 
-    assert isinstance(result, int), "O resultado deve ser um inteiro"
-
     result_upper = count_ocurrences(path, word.upper())
-    assert result == result_upper, "A contagem de palavras deve ser insensível a maiúsculas e minúsculas"
+    assert_msg = "A contagem de palavras deve ser insensível"
+    assert result == result_upper, assert_msg
