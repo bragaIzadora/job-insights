bd8203b2ad4f8b78cc4e808ad038ce19459bec9f
Izadora Braga <izadora_abraga@outlook.com>
2024-06-04 21:40:33 -0300

    requisito 8

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -23,7 +23,25 @@ class ProcessSalaries(ProcessJobs):
         return min(salaries) if salaries else None
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        if not all(key in job for key in ('min_salary', 'max_salary')):
+            raise ValueError("Informações de salário ausentes no emprego")
+
+        min_wage = self.str_to_int(job['min_salary'])
+        max_wage = self.str_to_int(job['max_salary'])
+        desired_salary = self.str_to_int(salary)
+
+        if max_wage < min_wage:
+            raise ValueError("Salário máximo é menor que o salário mínimo")
+
+        return min_wage <= desired_salary <= max_wage
+
+    def str_to_int(self, value: Union[int, str]) -> int:
+        if isinstance(value, str) and value.isdigit():
+            return int(value)
+        elif isinstance(value, int):
+            return value
+        else:
+            raise ValueError("Valor não numérico fornecido")
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
